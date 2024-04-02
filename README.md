e5316216f014c0ef46ad00b02a312b8146d90f89
Gleb Naumov <64691935+glenau@users.noreply.github.com>
2024-04-02 13:25:49 +0300

    Added templates

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # awesome-backend
+
 CLI tool for generating server side on Node.js
+
+## Installation
+
+This is a Node.js module available through the npm registry.
+
+Before installing, [download and install Node.js](https://nodejs.org/en/download/). Node.js 20 or higher is required.
+
+Installation is done using the npx command:
+
+```sh
+npx awesome-backend
+```
+
+## Features
+
+-
+
+## License
+
+[MIT](LICENSE)
--- bin/cli.js
+++ bin/cli.js
@@ -8,14 +8,19 @@
 import Questions from '../src/core/questions.js';
 import Generation from '../src/core/generation.js';
 import os from 'os';
+import chalk from 'chalk';
 
 async function run() {
     const nodeVersion = process.version;
     const majorVersion = parseInt(nodeVersion.slice(1), 10);
 
     if (majorVersion < 20) {
-        console.error('The minimum required Node.js version is 20 or higher. Please update your version and try again.');
+        console.log(chalk.yellow('The minimum required Node.js version is 20 or higher. Please update your version and try again.'));
         return;
+    } else {
+        console.log(
+            chalk.blue.bold('Welcome, traveler! Please answer the following questions, and I will help you generate the server-side part:')
+        );
     }
 
     const platform = os.platform();
@@ -24,7 +29,7 @@ async function run() {
         const answers = await Questions.start();
         const result = await new Generation(answers).start();
     } catch (error) {
-        console.log(error);
+        console.log(chalk.red.bold(error));
     }
 }
 
